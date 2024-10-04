af9d9b5ed4c8314544adc890167f6213f0cf49f3
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-10-03 23:03:38 -0300

    Utilizando plugins no tailwind : forms plugins.

--- aula17/src/app/produtos/cad-produto/page.tsx
+++ aula17/src/app/produtos/cad-produto/page.tsx
@@ -51,42 +51,42 @@ export default function CadProduto() {
         <h1>Cadastro de Produtos</h1>
 
         <div>
-            <form onSubmit={handleSubmit}>
-                <div>
+            <form onSubmit={handleSubmit} className="form">
+                <div className="div">
                     <label htmlFor="idNome">Nome Produto:</label>
                     <input type="text" id="idNome" name="nome" value={produto.nome} onChange={(e)=> setProduto({...produto,nome:e.target.value})}/>
                 </div>
-                <div>
+                <div className="div">
                     <label htmlFor="idPreco">Preço Produto:</label>
                     <input type="number" id="idPreco" name="preco" value={produto.preco} onChange={(e)=> setProduto({...produto,preco: parseFloat(e.target.value)})}/>
                 </div>
                 
-                <div>
+                <div className="div">
                     <label htmlFor="idMarca">Marca Produto:</label>
                     <input type="text" id="idMarca" name="marca" value={produto.marca} onChange={(e)=> setProduto({...produto,marca: e.target.value})}/>
                 </div>
 
-                <div>
+                <div className="div">
                     <label htmlFor="idCor">Cor Produto:</label>
                     <input type="text" id="idCor" name="cor" value={produto.cor} onChange={(e)=> setProduto({...produto,cor: e.target.value})}/>
                 </div>
 
-                <div>
+                <div className="div">
                     <label htmlFor="idModelo">Modelo Produto:</label>
                     <input type="text" id="idModelo" name="modelo" value={produto.modelo} onChange={(e)=> setProduto({...produto,modelo: e.target.value})}/>
                 </div>
 
-                <div>
+                <div className="div">
                     <label htmlFor="idQtd">Quantidade Produto:</label>
                     <input type="number" id="idQtd" name="quantidade" value={produto.quantidade} onChange={(e)=> setProduto({...produto,quantidade: parseInt(e.target.value)})}/>
                 </div>
 
-                <div>
+                <div className="div">
                     <label htmlFor="idCat">Categoria Produto:</label>
                     <input type="text" id="idCat" name="categoria" value={produto.categoria} onChange={(e)=> setProduto({...produto,categoria: e.target.value})}/>
                 </div>
 
-                <div>
+                <div className="div">
                     <label htmlFor="idImg">Imagem Produto:</label>
                     <input type="text" id="idImg" name="imagem" value={produto.imagem} onChange={(e)=> setProduto({...produto,imagem: e.target.value})}/>
                 </div>
